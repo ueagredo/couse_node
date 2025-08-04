043dab66d015c1e6428c8d2641e74f48744283e1
ueagredo <uveimar.159@gmail.com>
2025-08-04 11:32:45 -0500

    prueba de post

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
 
-const PORT = process.env.PORT || 3000
+const dovenv = require('dotenv').config()
+const express = require('express')
+
+
+
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3005
+
+
+console.log('la aplicacion esta funcionando en el puerto', PORT);
 
 app.get('/', (req,res) => {
     res.send(`
@@ -11,6 +21,50 @@ app.get('/', (req,res) => {
         `);
 });
 
+app.get('/users/:id', (req,res) => {
+    const usedId = req.params.id;
+    res.send(`El id del usuario es: ${usedId}`);
+}
+)
+
+app.get('/search',(req, res) => {
+    const termns = req.query.termino || 'no especificado';
+    const category = req.query.categoria || 'Todas';
+
+    res.send(`<h2>Buscando: ${termns}</h2>
+              <h3>Categoria: ${category}</h3>
+              <p>Resultados de la busqueda</p>`);
+    })
+
+
+app.post('/forms', (req, res) => {
+
+    const name = req.body.nombre || 'no especificado';
+    const email = req.body.email || 'no especificado';
+
+    res.json({
+        message: 'Datos recibidos correctamente',
+        data: {
+            name,
+            email
+        }
+    })
+})
+
+app.post('/api/data', (req, res) => {
+const data = req.body || {};
+console.log('Headers:', req.headers);
+console.log('Datos recibidos:', req.body);
+    if(!data || Object.keys(data).length === 0) {
+        return res.status(400).json({ error: 'No se recibieron datos' });
+    }
+
+    res.status(201).json({
+        message: 'Datos recibidos correctamente',
+        data: data
+    })
+})
+
 app.listen(PORT, () => {
-    console.log('la aplicacion esta funcionando');
+    console.log('servidor http://localhost:' + PORT);
 });
